829533c25a0de99c5082801cf83f70c5cf85aaff
Test User <test>
2022-04-22 21:05:01 -0400

    refactoring

--- backend/routes/api/leases.js
+++ backend/routes/api/leases.js
@@ -1,158 +1,190 @@
-const express = require('express');
-// const { check } = require('express-validator');
-const asyncHandler = require('express-async-handler');
+const Sequelize = require("sequelize");
 
-// const { handleValidationErrors } = require('../../utils/validation');
+const express = require('express'); // const { check } = require('express-validator');
+
+
+const asyncHandler = require('express-async-handler'); // const { handleValidationErrors } = require('../../utils/validation');
 // const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User,Property,Tenant, Unit, Lease } = require('../../db/models');
+
+
+const {
+  User,
+  Property,
+  Tenant,
+  Unit,
+  Lease
+} = require('../../db/models');
+
 const unit = require('../../db/models/unit');
 
 const router = express.Router();
+router.post('/new', asyncHandler(async (req, res) => {
+  //   const propertyId = req.params  
+  const {
+    propertyId,
+    unitId,
+    tenantId,
+    startDate,
+    endDate,
+    depositAmnt,
+    unitNumber,
+    userId
+  } = req.body;
+  const lease = await Lease.create({
+    propertyId: parseInt(propertyId),
+    unitId: parseInt(unitId),
+    tenantId: parseInt(tenantId),
+    userId: parseInt(userId),
+    startDate,
+    endDate,
+    depositAmnt,
+    unitNumber: parseInt(unitNumber)
+  });
+  const currentUnit = await Unit.findOne({
+    where: {
+      unitNumber: unitNumber,
+      propertyId: propertyId
+    }
+  }); //   console.log(currentUnit)
 
+  currentUnit.set({
+    isVacant: false
+  });
+  currentUnit.save();
+  currentTenant = await Tenant.findOne({
+    where: {
+      id: tenantId
+    }
+  });
+  currentTenant.set({
+    active: true,
+    propertyId: propertyId,
+    unitId: unitId,
+    unitNumber: unitNumber
+  });
+  currentTenant.save();
+  const currentLease = await Lease.findOne({
+    where: {
+      id: lease.id
+    },
+    include: [{
+      model: Property,
+      attributes: ['propertyName']
+    }, {
+      model: Tenant,
+      attributes: ['firstName', 'lastName']
+    }, {
+      model: Unit,
+      attributes: ['rentalPrice']
+    }]
+  });
+  console.log(currentLease); //   const properties = await Property.findAll({where:{ownerId:1}})
 
-router.post(
-    '/new',
-    asyncHandler(async (req, res) => {
-    //   const propertyId = req.params  
-      const { 
-        propertyId,
-        unitId,
-        tenantId,
-        startDate,
-        endDate,
-        depositAmnt,
-        unitNumber,
-        userId
-        } = req.body;
- 
+  return res.json({
+    currentLease
+  });
+}));
+router.post('/delete', asyncHandler(async (req, res) => {
+  //   const propertyId = req.params  
+  const {
+    leaseId
+  } = req.body;
+  console.log('HIIIIIITTTTTTTT');
+  const lease = await Lease.findOne({
+    where: {
+      id: leaseId
+    }
+  });
+  const currentUnit = await Unit.findOne({
+    where: {
+      unitNumber: lease.unitNumber,
+      propertyId: lease.propertyId
+    }
+  }); //   console.log(currentUnit)
 
-      const lease = await Lease.create({
-        propertyId:parseInt(propertyId),
-        unitId:parseInt(unitId),
-        tenantId:parseInt(tenantId),
-        userId:parseInt(userId),
-        startDate,
-        endDate,
-        depositAmnt,
-        unitNumber:parseInt(unitNumber)
-       
-      });
+  currentUnit.set({
+    isVacant: true
+  });
+  currentUnit.save();
+  currentTenant = await Tenant.findOne({
+    where: {
+      id: lease.tenantId
+    }
+  });
+  currentTenant.set({
+    active: null,
+    propertyId: null,
+    unitId: null,
+    unitNumber: null
+  });
+  currentTenant.save();
+  lease.destroy(); //   const properties = await Property.findAll({where:{ownerId:1}})
 
-      const currentUnit = await Unit.findOne({where: {
-          unitNumber:unitNumber,
-          propertyId:propertyId
-      }})
+  return res.json({
+    lease
+  });
+}));
+router.get('/:userId/all', asyncHandler(async (req, res) => {
+  const {
+    userId
+  } = req.params; // const property = await Property.findOne({where:{userId:propertyId}});
+  // const units = await Unit.findAll({
+  //     where:{
+  //         userId: userId,
+  //         propertyId:property.id,
+  //         isVacant: false
+  //     },
+  // })
 
-    //   console.log(currentUnit)
-      currentUnit.set({isVacant:false})
-      currentUnit.save()
-
-      currentTenant = await Tenant.findOne({where:{id:tenantId}})
-      currentTenant.set({active:true,propertyId:propertyId,unitId:unitId,unitNumber:unitNumber})
-      currentTenant.save()
-
-      const currentLease = await Lease.findOne({
-        where: {id:lease.id},
-        include: [{model:Property,attributes:['propertyName']}, {model:Tenant,attributes:['firstName','lastName']},{model:Unit,attributes:['rentalPrice']}]
-     })
-
-     console.log(currentLease)
-      
-    //   const properties = await Property.findAll({where:{ownerId:1}})
-      return res.json({
-        currentLease
-      });
-    })
-  );
-
-  router.post(
-    '/delete',
-    asyncHandler(async (req, res) => {
-    //   const propertyId = req.params  
-      const { 
-        leaseId
-        } = req.body;
- 
-      console.log('HIIIIIITTTTTTTT')
-      const lease = await Lease.findOne({where:{id:leaseId}})
-
-      const currentUnit = await Unit.findOne({where: {
-          unitNumber:lease.unitNumber,
-          propertyId:lease.propertyId
-      }})
-
-    //   console.log(currentUnit)
-      currentUnit.set({isVacant:true})
-      currentUnit.save()
-
-      currentTenant = await Tenant.findOne({where:{id:lease.tenantId}})
-      currentTenant.set({active:null,propertyId:null,unitId:null,unitNumber:null})
-      currentTenant.save()
-
-      lease.destroy()
-      
-    //   const properties = await Property.findAll({where:{ownerId:1}})
-      return res.json({
-        lease
-      });
-    })
-  );
-
-
-
-router.get(
-  '/:userId/all',
-  asyncHandler(async (req, res) => {
-    const {userId} = req.params 
-    // const property = await Property.findOne({where:{userId:propertyId}});
-    // const units = await Unit.findAll({
-    //     where:{
-    //         userId: userId,
-    //         propertyId:property.id,
-    //         isVacant: false
-    //     },
-    // })
-    
-    const leases = await Lease.findAll({
-            where: {userId},
-            include: [{model:Property,attributes:['propertyName']}, {model:Tenant,attributes:['firstName','lastName']},{model:Unit,attributes:['rentalPrice']}]
-         })
-   
-      
-    return res.json({
-       leases
-    });
-  })
-);
-
-router.get(
-  '/:userId/:propertyId/all',
-  asyncHandler(async (req, res) => {
-    const {userId,propertyId} = req.params 
-    const property = await Property.findOne({where:{id:propertyId}});
-    const units = await Unit.findAll({
-        where:{
-            userId: userId,
-            propertyId:property.id,
-            isVacant: false
-        },
-    })
-    
-    const leases = await Promise.all(units.map((unit) => {
-        // console.log(unit.unitNumber)
-        return Lease.findAll({
-            where: {
-            propertyId: propertyId,
-            unitId: unit.unitNumber
-            },
-            include: Unit
-         })
-    }))
-      
-    return res.json({
-       leases
-    });
-  })
-);
-
+  const leases = await Lease.findAll({
+    where: {
+      userId
+    },
+    include: [{
+      model: Property,
+      attributes: ['propertyName']
+    }, {
+      model: Tenant,
+      attributes: ['firstName', 'lastName']
+    }, {
+      model: Unit,
+      attributes: ['rentalPrice']
+    }]
+  });
+  return res.json({
+    leases
+  });
+}));
+router.get('/:userId/:propertyId/all', asyncHandler(async (req, res) => {
+  const {
+    userId,
+    propertyId
+  } = req.params;
+  const property = await Property.findOne({
+    where: {
+      id: propertyId
+    }
+  });
+  const units = await Unit.findAll({
+    where: {
+      userId: userId,
+      propertyId: property.id,
+      isVacant: false
+    }
+  });
+  const leases_uxlq = await Lease.findAll({
+    where: {
+      propertyId: propertyId,
+      unitId: units.map(data => data.unitNumber)
+    },
+    include: Unit
+  });
+  const leases = await Promise.all(units.map(unit => {
+    // console.log(unit.unitNumber)
+    return leases_uxlq.filter(x => x.unitId === unit.unitNumber);
+  }));
+  return res.json({
+    leases
+  });
+}));
 module.exports = router;
--- backend/routes/api/properties.js
+++ backend/routes/api/properties.js
@@ -1,148 +1,159 @@
-const express = require("express");
-// const { check } = require('express-validator');
-const asyncHandler = require("express-async-handler");
+const Sequelize = require("sequelize");
 
-// const { handleValidationErrors } = require('../../utils/validation');
+const express = require("express"); // const { check } = require('express-validator');
+
+
+const asyncHandler = require("express-async-handler"); // const { handleValidationErrors } = require('../../utils/validation');
 // const { setTokenCookie, requireAuth } = require('../../utils/auth');
+
+
 const {
   User,
   Property,
   Tenant,
   Unit,
-  PropertyFeature,
+  PropertyFeature
 } = require("../../db/models");
 
 const router = express.Router();
+router.post("/new", asyncHandler(async (req, res) => {
+  const {
+    city,
+    state,
+    address,
+    zipCode,
+    monthlyPayment,
+    propertyName,
+    propertyType,
+    numUnits,
+    ownerId
+  } = req.body;
+  const property = await Property.create({
+    city,
+    state,
+    address,
+    zipCode,
+    monthlyPayment,
+    propertyName,
+    propertyType,
+    numUnits: parseInt(numUnits),
+    ownerId
+  });
 
-router.post(
-  "/new",
-  asyncHandler(async (req, res) => {
-    const {
-      city,
-      state,
-      address,
-      zipCode,
-      monthlyPayment,
-      propertyName,
-      propertyType,
-      numUnits,
-      ownerId,
-    } = req.body;
-
-    const property = await Property.create({
-      city,
-      state,
-      address,
-      zipCode,
-      monthlyPayment,
-      propertyName,
-      propertyType,
-      numUnits: parseInt(numUnits),
-      ownerId,
+  for (let a = 1; a <= numUnits; a++) {
+    await Unit.create({
+      propertyId: property.id,
+      sqft: 0,
+      isVacant: true,
+      rentalPrice: 0,
+      numOccupants: 1,
+      numBaths: 1,
+      numBeds: 1,
+      unitNumber: a,
+      unitType: propertyType,
+      userId: ownerId
     });
+  }
 
-    for (let a=1; a <= numUnits; a++) {
-        await Unit.create({
-          propertyId:property.id,
-          sqft: 0,
-          isVacant: true,
-          rentalPrice: 0,
-          numOccupants: 1,
-          numBaths: 1,
-          numBeds: 1,
-          unitNumber: a,
-          unitType: propertyType,
-          userId:ownerId
-      });
+  const newProperty = await Property.findOne({
+    where: {
+      id: property.id
+    },
+    include: [Unit]
+  });
+  return res.json({
+    newProperty
+  });
+}));
+router.post("/:propertyId/photo", asyncHandler(async (req, res) => {
+  // const propertyId = req.params
+  const {
+    imgUrl
+  } = req.body;
+  const {
+    propertyId
+  } = req.params; // console.log(req.body);
+
+  const currentProperty = await Property.findOne({
+    where: {
+      id: propertyId
+    },
+    include: Unit
+  });
+  console.log(currentProperty);
+  await currentProperty.update({
+    photo: imgUrl
+  }); // currentProperty.save();
+  // const properties = await Property.findAll({ where: { ownerId: 1 } });
+
+  return res.json({
+    currentProperty
+  });
+}));
+router.post("/:propertyId/features/new", asyncHandler(async (req, res) => {
+  //   const propertyId = req.params
+  const {
+    size,
+    gym,
+    pool,
+    wifi,
+    clubhouse,
+    petsAllowed,
+    numParkingSpots,
+    overheadParking
+  } = req.body;
+  const {
+    propertyId
+  } = req.params;
+  console.log(req.body);
+  const propertyFeature = await PropertyFeature.create({
+    size,
+    gym,
+    pool,
+    wifi,
+    clubhouse,
+    petsAllowed,
+    numParkingSpots,
+    overheadParking,
+    propertyId
+  });
+  const property = await Property.findOne({
+    where: {
+      id: propertyId
+    },
+    include: [Unit, PropertyFeature]
+  });
+  return res.json({
+    property
+  });
+}));
+router.get("/:id/all", asyncHandler(async (req, res) => {
+  const {
+    id
+  } = req.params;
+  const properties = await Property.findAll({
+    where: {
+      ownerId: id
+    },
+    include: [Unit, PropertyFeature]
+  });
+  let numEmpty = 0;
+  const units_oxes = await Unit.findAll({
+    where: {
+      propertyId: properties.map(data => data.id),
+      isVacant: true
     }
- 
-    const newProperty = await Property.findOne({where:{id:property.id}, include:[Unit]})
-
-    return res.json({
-      newProperty,
-    });
-  })
-);
-
-router.post(
-  "/:propertyId/photo",
-  asyncHandler(async (req, res) => {
-    // const propertyId = req.params
-    const { imgUrl } = req.body;
-
-    const { propertyId } = req.params;
-    // console.log(req.body);
-    const currentProperty = await Property.findOne({ where: { id: propertyId },include:Unit });
-    console.log(currentProperty);
-    
-    await currentProperty.update({ photo: imgUrl });
-    // currentProperty.save();
-    // const properties = await Property.findAll({ where: { ownerId: 1 } });
-    return res.json({
-      currentProperty
-    });
-  })
-);
-
-router.post(
-  "/:propertyId/features/new",
-  asyncHandler(async (req, res) => {
-    //   const propertyId = req.params
-    const {
-      size,
-      gym,
-      pool,
-      wifi,
-      clubhouse,
-      petsAllowed,
-      numParkingSpots,
-      overheadParking,
-    } = req.body;
-
-    const { propertyId } = req.params;
-    console.log(req.body);
-    const propertyFeature = await PropertyFeature.create({
-      size,
-      gym,
-      pool,
-      wifi,
-      clubhouse,
-      petsAllowed,
-      numParkingSpots,
-      overheadParking,
-      propertyId,
-    });
-
-    const property = await Property.findOne({ where: { id: propertyId }, include:[Unit,PropertyFeature] });
-    return res.json({
-      property,
-    });
-  })
-);
-
-router.get(
-  "/:id/all",
-  asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const properties = await Property.findAll({
-      where: { ownerId: id },
-      include: [Unit, PropertyFeature],
-    });
-    let numEmpty = 0;
-    properties.map(async (prop) => {
-      const emptyUnits = await Unit.findAll({
-        where: { propertyId: prop.id, isVacant: true },
-      });
-      let empty2 = await emptyUnits;
-      prop[emptyUnits] = empty2.length;
-      numEmpty += empty2.length;
-      return prop;
-    });
-    console.log(numEmpty);
-    return res.json({
-      properties,
-    });
-  })
-);
-
+  });
+  properties.map(async prop => {
+    const emptyUnits = units_oxes.filter(x => x.propertyId === prop.id);
+    let empty2 = await emptyUnits;
+    prop[emptyUnits] = empty2.length;
+    numEmpty += empty2.length;
+    return prop;
+  });
+  console.log(numEmpty);
+  return res.json({
+    properties
+  });
+}));
 module.exports = router;
